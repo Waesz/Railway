198942b4e704645cca28d51233244cbe92ccdefd
Mustapha <mustafatourgi00@gmail.com>
2020-12-04 02:51:12 +0100

    Ticket
    interface + code (manque sql)

---
+++ FinalValid.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FinalValid implements Initializable {
+	@FXML private Label Nompre;
+	@FXML private Label VilleD;
+	@FXML private Label VilleA;
+	@FXML private Label HeureD;
+	@FXML private Label Date;
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
